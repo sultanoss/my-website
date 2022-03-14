1982cd1e3f485945274b93a0bf6d4fadba60c97d
sultanoss <radwansultan@hotmail.dem>
2022-03-14 19:20:14 +0100

    Added skill-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { MenuComponent } from './menu/menu.component';
+import { MyskillsComponent } from './myskills/myskills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    MenuComponent
+    MenuComponent,
+    MyskillsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/myskills/myskills.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyskillsComponent } from './myskills.component';
+
+describe('MyskillsComponent', () => {
+  let component: MyskillsComponent;
+  let fixture: ComponentFixture<MyskillsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MyskillsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyskillsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
