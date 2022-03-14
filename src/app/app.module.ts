13f7f56939c62d5900c6630393155b06e498b02d
sultanoss <radwansultan@hotmail.dem>
2022-03-14 17:15:14 +0100

    Added banner/menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
