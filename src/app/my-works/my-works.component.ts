4ffad24dfbb9946e58934bfed06cdcd448aca9e0
sultanoss <radwansultan@hotmail.dem>
2022-03-23 01:40:13 +0100

    Added myskills hover-effect

--- src/app/my-works/my-works.component.ts
+++ src/app/my-works/my-works.component.ts
@@ -8,6 +8,14 @@ import { Router } from '@angular/router';
 })
 export class MyWorksComponent implements OnInit {
 
+  changeText:boolean = false;
+  changeText2:boolean = false;
+  changeText3:boolean = false;
+  changeText4:boolean = false;
+  changeText5:boolean = false;
+  changeText6:boolean = false;
+  changeText7:boolean = false;
+  changeText8:boolean = false;
   constructor(public router:Router) { }
 
   ngOnInit(): void {
